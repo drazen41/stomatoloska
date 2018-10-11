47ac376a3fc1c91032f6b8f29aa30d3d6357ed8f
drazen41 <drazen.svec@gmail.com>
2018-10-11 14:16:46 +0200

    Inicijalno

---
+++ Stomatoloska/Stomatoloska.Webforms/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Stomatoloska.Webforms.Global" Language="C#" %>
---
+++ Stomatoloska/Stomatoloska.Webforms/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Stomatoloska.Webforms.Startup))]
+namespace Stomatoloska.Webforms
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
