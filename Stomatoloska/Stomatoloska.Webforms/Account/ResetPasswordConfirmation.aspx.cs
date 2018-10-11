0bf7bb5e592abe1f562816274e17c7b9d2e5664f
drazen41 <drazen.svec@gmail.com>
2018-10-11 14:25:44 +0200

    deleted

--- Stomatoloska/Stomatoloska.Webforms/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace Stomatoloska.Webforms.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- Stomatoloska/Stomatoloska.Webforms/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Stomatoloska.Webforms.Global" Language="C#" %>
